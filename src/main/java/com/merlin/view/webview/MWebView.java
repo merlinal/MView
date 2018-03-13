b5e6e2b6002dbf383c069cd5bf8960b1e3c6de75
赵安林 <zhaoal@cnsuning.com>
2018-03-13 15:46:55 +0800

    webview

---
+++ src/main/java/com/merlin/view/webview/MWebView.java
@@ -0,0 +1,39 @@
+package com.merlin.view.webview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.webkit.WebView;
+
+/**
+ * @author merlin
+ */
+
+public class MWebView extends WebView {
+
+    public MWebView(Context context) {
+        this(context, null);
+    }
+
+    public MWebView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        //解决： ViewPager里非首屏WebView点击事件不响应
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            onScrollChanged(getScrollX(), getScrollY(), getScrollX(), getScrollY());
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    public boolean existVerticalScrollbar() {
+        return computeVerticalScrollExtent() < computeVerticalScrollRange();
+    }
+
+}
